5ce1503d48f8567cfc4b4d7715b17e2a3471f6ec
Fatma Tawfeek <fatmatawfeek97@gmail.com>
2023-09-19 13:22:08 +0300

    first commit

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,46 @@
+<!-- Title -->
+<title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
+<!-- Favicon -->
+<link rel="icon" href="{{URL::asset('assets/img/brand/favicon.png')}}" type="image/x-icon"/>
+<!-- Icons css -->
+<link href="{{URL::asset('assets/css/icons.css')}}" rel="stylesheet">
+<!--  Custom Scroll bar-->
+<link href="{{URL::asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+<!--  Sidebar css -->
+<link href="{{URL::asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+
+@if (app()->getLocale() == 'ar')
+
+<!-- Sidemenu css -->
+<link rel="stylesheet" href="{{URL::asset('assets/css-rtl/sidemenu.css')}}">
+@yield('css')
+<!--- Style css -->
+<link href="{{URL::asset('assets/css-rtl/style.css')}}" rel="stylesheet">
+<!--- Dark-mode css -->
+<link href="{{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
+<!---Skinmodes css-->
+<link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+{{-- font --}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+<style>
+    body, h1, h2, h3, h4, h5, h6{
+        font-family: 'IBM Plex Sans Arabic', sans-serif !important;
+    }
+</style>
+    
+@else
+
+<!-- Sidemenu css -->
+<link rel="stylesheet" href="{{URL::asset('assets/css/sidemenu.css')}}">
+@yield('css')
+<!--- Style css -->
+<link href="{{URL::asset('assets/css/style.css')}}" rel="stylesheet">
+<!--- Dark-mode css -->
+<link href="{{URL::asset('assets/css/style-dark.css')}}" rel="stylesheet">
+<!---Skinmodes css-->
+<link href="{{URL::asset('assets/css/skin-modes.css')}}" rel="stylesheet">
+    
+@endif
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+], function () {
+
+    Route::get('/', function () {
+        return view('index');
+    });
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    require __DIR__.'/auth.php';
+
+});
+
+
+
+
