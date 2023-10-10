0310f0ff8d642d6b35f478c6d7d2f1a90056aa87
Fatma Tawfeek <fatmatawfeek97@gmail.com>
2023-10-10 07:24:05 +0300

    Add 403 error page

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,30 @@
-@extends('errors::minimal')
+@extends('layouts.master2')
 
-@section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('title')
+الصفحة محظورة
+@endsection
+
+@section('css')
+<!--- Internal Fontawesome css-->
+<link href="{{URL::asset('assets/plugins/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+<!---Ionicons css-->
+<link href="{{URL::asset('assets/plugins/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+<!---Internal Typicons css-->
+<link href="{{URL::asset('assets/plugins/typicons.font/typicons.css')}}" rel="stylesheet">
+<!---Internal Feather css-->
+<link href="{{URL::asset('assets/plugins/feather/feather.css')}}" rel="stylesheet">
+<!---Internal Falg-icons css-->
+<link href="{{URL::asset('assets/plugins/flag-icon-css/css/flag-icon.min.css')}}" rel="stylesheet">
+@endsection
+@section('content')
+		<!-- Main-error-wrapper -->
+		<div class="main-error-wrapper  page page-h ">
+			<img src="{{URL::asset('assets/img/media/403.png')}}" class="error-page" alt="error">
+			<h2>الصفحة التي تبحث عنها محظورة.</h2>
+			<h6>ليس لديك الصلاحية لزيادة هذه الصفحة . يرجي الرجوع الي مدير الموقع.</h6>
+			<a class="btn btn-outline-danger" href="{{ route('home') }}">العودة الى الرئيسية</a>
+		</div>
+		<!-- /Main-error-wrapper -->
+@endsection
+@section('js')
+@endsection
